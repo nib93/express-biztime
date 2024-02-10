9f44f0a901be271ae1740e6f1f814c70bc6e8f6f
Neeti <neeti.i.bhatt@gmail.com>
2024-02-10 01:19:07 -0800

    updating all the files

--- app.js
+++ app.js
@@ -1,24 +1,82 @@
-/** BizTime express application. */
-
-
-const express = require("express");
-
+const express = require('express');
 const app = express();
-const ExpressError = require("./expressError")
+const ExpressError = require('./expressError');
 
-app.use(express.json());
+const { convertAndValidateNumsArray, findMode, findMean, findMedian } = require('./helpers');
+
+app.get('/mean', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
 
 
-/** 404 handler */
+  let result = {
+    operation: "mean",
+    result: findMean(nums)
+  }
 
-app.use(function(req, res, next) {
-  const err = new ExpressError("Not Found", 404);
+  return res.send(result);
+});
+
+app.get('/median', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+  let result = {
+    operation: "median",
+    result: findMedian(nums)
+  }
+
+  return res.send(result);
+  
+});
+
+app.get('/mode', function(req, res, next) {
+  if (!req.query.nums) {
+    throw new ExpressError('You must pass a query key of nums with a comma-separated list of numbers.', 400)
+  }
+  let numsAsStrings = req.query.nums.split(',');
+  // check if anything bad was put in
+  let nums = convertAndValidateNumsArray(numsAsStrings);
+  if (nums instanceof Error) {
+    throw new ExpressError(nums.message);
+  }
+
+  let result = {
+    operation: "mode",
+    result: findMode(nums)
+  }
+
+  return res.send(result);
+
+ 
+});
+
+/** general error handler */
+
+app.use(function (req, res, next) {
+  const err = new ExpressError("Not Found",404);
+
+  // pass the error to the next piece of middleware
   return next(err);
 });
 
 /** general error handler */
 
-app.use((err, req, res, next) => {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
 
   return res.json({
@@ -28,4 +86,6 @@ app.use((err, req, res, next) => {
 });
 
 
-module.exports = app;
+app.listen(3000, function() {
+  console.log(`Server starting on port 3000`);
+});
